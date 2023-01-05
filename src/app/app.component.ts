8a3ae401082c5f84d8de6abf53686796d6a5f94b
Kurt Yang <kurt_yang@kunyoutech.com.tw>
2023-01-05 11:03:29 +0800

    [form] basic usage done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,34 @@ export class AppComponent {
   defaultQuestion: string = 'pet';
   answer: '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted= false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // setValue() 可以替 form 表單設定 value
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //   },
+    //   email:  "",
+    //   secret: 'pet',
+    //   questionAnswer: "",
+    //   gender: 'male'
+    // })
+
+    // .form.patchValue 可以只覆蓋特定 form 元素或是 form group 資料
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // 單純使用 HTML 操控表單
@@ -28,6 +53,14 @@ export class AppComponent {
 
   // 透過 @ViewChild reference 操控表單
   onSubmit() {
-    console.log(this.signupForm)
+    this.submitted= true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    // .reset() 可以重設表單 可以傳遞 參數給預設值，方法同 setValue()
+    this.signupForm.reset();
   }
 }
